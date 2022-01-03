ffcb22dacf17e548f1f4c916c5f490e6dd21bd96
Jefferson Alquinga <55715332+Jeffers1998@users.noreply.github.com>
2022-01-03 09:19:07 -0500

    Puzzle 5,10,15
    Ejercicios de AdventOfCode2021

---
+++ Día 10/Día10_puzzle 1 solución.py
@@ -0,0 +1,43 @@
+"""
+Created on Mon Jan  2 10:02:57 2022
+
+@author: Jefferson
+"""
+
+
+def mypuzzle(route:list):
+    input = {"(","[","<","{"}
+    aux = {")":"(","]":"[",">":"<","}":"{"}  
+    score = {"}":1197,")":3,"]":57,">":25137}
+    total = 0
+    myStack = []
+    for i in range(len(route)):
+        for j in range(len(route[i])):
+            if route[i][j] in input:
+                myStack.append(route[i][j])
+            else:    
+                if len(myStack) == 0:
+                    total += score[route[i][j]]
+                    break
+    
+                elif myStack[-1] == aux[route[i][j]]:
+                    myStack.pop() 
+                else:
+                    total += score[route[i][j]]
+                    break
+
+        myStack = []
+    return total
+
+   
+  
+def getList():
+    with open("Day10\day10_puzzle1_list.txt") as f:
+        return [str(line.strip()) for line in f]
+
+# def getList():
+#     with open("Day10\\test.txt") as f:
+#         return [str(line.strip()) for line in f]
+
+
+print(mypuzzle(getList()))
